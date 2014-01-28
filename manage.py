02df0346dbfbd54a1961a78b8d4942286895f887
Kyla Farrell <kylafarrell@gmail.com>
2014-01-28 19:58:35 +0000

    migrated database

--- manage.py
+++ manage.py
@@ -14,6 +14,19 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+class Area(db.Model):
+    __tablename__ = 'area'
+    id = db.Column(db.Integer, primary_key = True)
+    area = db.Column(db.String(3))
+    location = db.Column(db.String(3))
+
+    def __init__(self, area, location):
+        self.area = area
+        self.location = location
+
+    def __repr__(self):
+        return '<Area %r>' % self.area
+
 class Track(db.Model):
     __tablename__ = 'track'
     id = db.Column(db.Integer, primary_key=True)
@@ -22,31 +35,20 @@ class Track(db.Model):
     station_start = db.Column(db.Float)
     station_end = db.Column(db.Float)
     quantity = db.Column(db.Float)
-    area_id = db.Column(db.Integer, db.ForeignKey('area.id'))
 
-    def __init__(self, timestamp, date, station_start, station_end, quantity):
+    area_id = db.Column(db.Integer, db.ForeignKey('area.id'))
+    area = db.relationship('Area', backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, timestamp, date, station_start, station_end, quantity, area):
         self.timestamp = timestamp
         self.date = date
         self.station_start = station_start
         self.station_end = station_end
         self.quantity = quantity
+        self.area = area
 
     def __repr__(self):
         return '<Tracking %r>' % self.date
-
-class Area(db.Model):
-    __tablename__ = 'area'
-    id = db.Column(db.Integer, primary_key = True)
-    area = db.Column(db.String(3))
-    location = db.Column(db.String(3))
-    tracked_items = db.relationship("Track", backref="area")
-
-    def __init__(self, area, location):
-        self.area = area
-        self.location = location
-
-    def __repr__(self):
-        return '<Area %r>' % self.area
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class Area(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     area = db.Column(db.String(3))
     location = db.Column(db.String(3))
-    tracked_items = db.relationship("Track", backref='area')
 
     def __init__(self, area, location):
         self.area = area
@@ -22,15 +21,17 @@ class Track(db.Model):
     station_start = db.Column(db.Float)
     station_end = db.Column(db.Float)
     quantity = db.Column(db.Float)
-    area_id = db.Column(db.Integer, db.ForeignKey('area.id'))
 
-    def __init__(self, timestamp, date, station_start, station_end, quantity, area_id):
+    area_id = db.Column(db.Integer, db.ForeignKey('area.id'))
+    area = db.relationship('Area', backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, timestamp, date, station_start, station_end, quantity, area):
         self.timestamp = timestamp
         self.date = date
         self.station_start = station_start
         self.station_end = station_end
         self.quantity = quantity
-        self.area_id = area_id
+        self.area = area
 
     def __repr__(self):
         return '<Tracking %r>' % self.date
